06e1ccd987ec88bad93d8cbcc64319ffbbe7eb60
sw <walker_mansun@163.com>
2020-09-20 16:27:24 +0800

    this is the first commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: './src/app.tsx',
+    output: {
+        path: __dirname + '/public',
+        filename: 'build/app.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            { 
+                test: /\.(js|mjs|jsx|ts|tsx)$/, 
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+            }
+        ]
+    },    
+}
